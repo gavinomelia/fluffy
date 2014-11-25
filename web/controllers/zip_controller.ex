aa105d9489bdb10380dd456106db0c8e9bffaf2b
Gavin OMelia <gavin@omelia.org>
2014-11-24 22:43:10 -0500

    Add geocoding

--- web/controllers/zip_controller.ex
+++ web/controllers/zip_controller.ex
@@ -3,19 +3,18 @@ defmodule Fluffy.ZipController do
 
   plug :action
 
-  @coordinates %{
-    "27592" => "35.5957,-78.7314",
-    "06030" => "41.7278,-72.8403",
-    "02110" => "42.3581,-71.0636",
-    "73301" => "30.2500,-97.7500",
-    "29928" => "32.2163,-80.7526"
-  }
-
   def show(conn, %{"id" => id}) do
     { temp, summary, forecast, temperatureMax, temperatureMin, precipProbability } = weather_for(id)
     render conn, "show.html", id: id, temperature: temp, summary: summary, forecast: forecast, temperatureMax: temperatureMax, temperatureMin: temperatureMin, precipProbability: precipProbability
   end
 
+
+  defp lat_lng(zip) do
+    { :ok, parsed } = call_geocoding(zip) |> JSON.decode
+    first_result = parsed["results"] |> List.first
+    loc = first_result["geometry"]["location"]
+    ~s(#{loc["lat"]},#{loc["lng"]})
+  end
 
   defp weather_for(zip) do
     { :ok, parsed } = call_forecast_io(zip) |> JSON.decode
@@ -28,7 +27,11 @@ defmodule Fluffy.ZipController do
   end
 
   defp call_forecast_io(zip) do
-    get_response_body("https://api.forecast.io/forecast/7768b533541101aa661ff471f89c5dce/#{@coordinates[zip]}")
+    get_response_body("https://api.forecast.io/forecast/7768b533541101aa661ff471f89c5dce/#{lat_lng(zip)}")
+  end
+
+  defp call_geocoding(zip) do
+    get_response_body("https://maps.googleapis.com/maps/api/geocode/json?address=#{zip}&key=AIzaSyCnw9k4-E-OXJImTlrCsiz7Vlt5j9PK_ig")
   end
 
   defp get_response_body(url) do
