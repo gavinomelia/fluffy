8990a4f7bb6a355eebf11b3e4d8fd086fad0df51
Duff OMelia <duff@omelia.org>
2014-11-29 13:07:19 -0500

    Show location

--- web/controllers/zip_controller.ex
+++ web/controllers/zip_controller.ex
@@ -4,38 +4,35 @@ defmodule Fluffy.ZipController do
   plug :action
 
   def show(conn, %{"id" => id}) do
-    { temp, summary, forecast, temperatureMax, temperatureMin, precipProbability } = weather_for(id)
-    render conn, "show.html", id: id, temperature: temp, summary: summary, forecast: forecast, temperatureMax: temperatureMax, temperatureMin: temperatureMin, precipProbability: precipProbability
+    { address, temp, summary, forecast, temperatureMax, temperatureMin, precipProbability } = weather_for(id)
+    render conn, "show.html", address: address, temperature: temp, summary: summary,
+           forecast: forecast, temperatureMax: temperatureMax, temperatureMin: temperatureMin,
+           precipProbability: precipProbability
   end
 
 
-  defp lat_lng(zip) do
+  defp geocode(zip) do
     { :ok, parsed } = call_geocoding(zip) |> JSON.decode
     first_result = parsed["results"] |> List.first
     loc = first_result["geometry"]["location"]
-    ~s(#{loc["lat"]},#{loc["lng"]})
-  end
-
-  defp address(zip) do
-   { :ok, parsed } = call_geocoding(zip) |> JSON.decode
-   first_result = parsed["results"] |> List.first
-   IO.inspect first_result
-   address_thing = first_result["formatted_address"]
-   IO.inspect address_thing
+    address = first_result["formatted_address"]
+    lat_lng = ~s(#{loc["lat"]},#{loc["lng"]})
+    { address, lat_lng }
   end
 
   defp weather_for(zip) do
-    { :ok, parsed } = call_forecast_io(zip) |> JSON.decode
+    { address, lat_lng } = geocode(zip)
+    { :ok, parsed } = call_forecast_io(lat_lng) |> JSON.decode
     current = parsed["currently"]
     daily = parsed["daily"]
 
     key = daily["data"]
     first_key = List.first(key)
-    { current["temperature"], current["summary"], parsed["hourly"]["summary"], first_key["temperatureMax"], first_key["temperatureMin"], first_key["precipProbability"] }
+    { address, current["temperature"], current["summary"], parsed["hourly"]["summary"], first_key["temperatureMax"], first_key["temperatureMin"], first_key["precipProbability"] }
   end
 
-  defp call_forecast_io(zip) do
-    get_response_body("https://api.forecast.io/forecast/7768b533541101aa661ff471f89c5dce/#{lat_lng(zip)}")
+  defp call_forecast_io(lat_lng) do
+    get_response_body("https://api.forecast.io/forecast/7768b533541101aa661ff471f89c5dce/#{lat_lng}")
   end
 
   defp call_geocoding(zip) do
